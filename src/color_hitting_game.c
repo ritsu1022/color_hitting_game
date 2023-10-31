b92f39ccefcd921822d70b8634336641201c369f
ritsu1022 <128396288+ritsu1022@users.noreply.github.com>
2023-10-31 23:59:55 +0900

    予想を10回繰り返す

--- src/color_hitting_game.c
+++ src/color_hitting_game.c
@@ -22,24 +22,26 @@ int main(void) {
 	puts("【色当てゲーム】");
 	puts("ゲームをはじめてください");
 	puts("コンピュータが問題を出しました。");
-	puts("予想を入力してください。");
+	for(int i = 0; i < 10; i++) {
+		printf("予想を入力してください。%d 回目\n", i + 1);
 
-	char t1 = getchar();
-	char t2 = getchar();
-	char t3 = getchar();
-	char t4 = getchar();
+		char t1 = getchar();
+		char t2 = getchar();
+		char t3 = getchar();
+		char t4 = getchar();
 
-	putchar(t1);
-	putchar(t2);
-	putchar(t3);
-	putchar(t4);
+		putchar(t1);
+		putchar(t2);
+		putchar(t3);
+		putchar(t4);
 
-	int matched = 0;
-	if(q1 == t1) { matched += 1; }
-	if(q2 == t2) { matched += 1; }
-	if(q3 == t3) { matched += 1; }
-	if(q4 == t4) { matched += 1; }
-	puts("結果");
-	printf("%d コ合っています。\n", matched );
+		int matched = 0;
+		if(q1 == t1) { matched += 1; }
+		if(q2 == t2) { matched += 1; }
+		if(q3 == t3) { matched += 1; }
+		if(q4 == t4) { matched += 1; }
+		puts("結果");
+		printf("%d コ合っています。\n", matched );
+	}
 	return EXIT_SUCCESS;
 }
