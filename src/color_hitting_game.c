10e425e022e303cf2da15c4cf2d0632ddc49ff55
ritsu1022 <128396288+ritsu1022@users.noreply.github.com>
2023-11-29 17:59:45 +0900

    入力内容を確認する関数を組み込む

--- src/color_hitting_game.c
+++ src/color_hitting_game.c
@@ -11,6 +11,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+#include <ctype.h>
 #include <time.h>
 
 #define QSIZE 4
@@ -64,6 +66,64 @@ void chg_make_question(void) {
 #ifdef DEBUG
 	chg_display_question();
 #endif // DEBUG
+}
+
+bool chg_input_chars_is_no_dup(const char* buf) {
+	size_t length = strlen(buf);
+
+	int freq[num_of_colors];
+	memset(freq, 0, sizeof(int) * num_of_colors);
+
+	for (size_t i = 0; i < length; i++) {
+		char* offset = memchr(qseeds, toupper(buf[i]), num_of_colors);
+		if (offset != NULL) {
+			int idx = offset - qseeds;
+			freq[idx]++;
+		}
+	}
+
+	bool ret = true;
+	for (int i = 0; i < num_of_colors; i++) {
+		if (freq[i] >= 2) {
+			printf("同じ文字\"%c\"が%d回使われています\n", qseeds[i], freq[i]);
+			ret = false;
+		}
+	}
+	return ret;
+}
+
+bool chg_input_chars_is_valid(const char* buf) {
+	size_t length = strlen(buf);
+
+	char illegal_chars[QSIZE] = { '\0' };
+	int j = 0;
+	for (size_t i = 0; i < length; i++) {
+		char ch = toupper(buf[i]);
+		if (memchr(qseeds, ch, num_of_colors) == NULL) {
+			if (memchr(illegal_chars, buf[i],QSIZE) == NULL) {
+				illegal_chars[j++] = buf[i];
+			}
+		}
+	}
+	if (strlen(illegal_chars) > 0) {
+		printf("使えない文字\"%s\"が含まれています。\n", illegal_chars);
+		return false;
+	}
+	return true;
+}
+
+bool chg_input_length_is_valid(const char* buf) {
+	size_t length = strlen(buf);
+
+	if (length < QSIZE) {
+		puts("入力が短すぎます");
+		return false;
+	}
+	if (length > QSIZE) {
+		puts("入力が長すぎます");
+		return false;
+	}
+	return true;
 }
 
 void chg_input_answer(char buf[], int size) {
